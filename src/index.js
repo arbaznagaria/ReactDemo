5f5cc810c702f89ba2fa5f40008c2601384ad5a0
Arbaz Nagaria <arbaznagaria@gmail.com>
2022-03-02 22:34:09 +0530

    Calling js function in H1 tag

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
+function name(user){
+  return user.firstname + ' ' + user.lastname;
+}
+var user = {
+  firstname: "Arbaz",
+  lastname: "Nagaria"
+}
+var element = <h1>Hello, {name(user)}</h1>;
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  element,
   document.getElementById('root')
 );
